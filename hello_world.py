8c3dbae7824a949360f1cac33e5bfe6d20a6b363
Macbook Air <macbookair@MacBookAir.local>
2021-09-09 19:24:29 -0300

    first commit

---
+++ hello_world.py
@@ -0,0 +1,20 @@
+
+print("Hola mundo")
+
+
+name = "Leon"
+print("Hola" , name)
+
+print("Hola " + name)
+
+num = 7
+print("¡Hola" , num , "¡"  )
+
+print("Hola " + str(num) + "¡")
+    
+food_1 = "Salmon"
+food_2 = " potatoes" 
+
+print(f"Me encanta comer {food_1} y {food_2}")
+
+print("Me encanta comer {} y {} ".format (food_2 , food_1))
