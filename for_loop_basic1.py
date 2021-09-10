534e7568c2cdef333d992f5f3fa4298a9cf5140f
LeoBerezin <84946843+LeoBerezin@users.noreply.github.com>
2021-09-10 09:36:53 -0300

    Add files via upload

---
+++ for_loop_basic1.py
@@ -0,0 +1,44 @@
+#for x in range(151):
+
+     #print(x)
+ 
+#for y in range (0, 1005, 5):
+ #   print (y)
+    
+#import math    
+#for lista in range (1,101,1):
+    
+    #if (math.remainder(lista, 5)) == 0 and (math.remainder(lista, 10)) == 0 : 
+        #print ("Coding Dojo") 
+    #elif (math.remainder(lista, 5)) == 0 :
+        #print ("Coding ")
+    #else :
+        #print(lista)
+          
+ 
+#import math 
+#y = 0     
+#for x in range (0,500001,1):
+    #if (math.remainder(x,2)) != 0:
+ 
+        ##y = y + x  
+#print(y)
+         
+         
+
+#for x in range (2018, 0, -4):
+   #print (x) 
+ 
+ 
+#import math   
+#print("Contador flexible : establece tres variables: lowNum, highNum, mult. Comenzando en lowNum y pasando por highNum, imprima solo los enteros que son múltiplos de mult. ")
+#print("ingrese el numero menor")   
+#lowNum = input()
+#print("ingrese el numero mayor")
+#highNum = input()
+#print("ingrese el multiplo")
+#mult=input()
+
+#for r in range (int(lowNum),int(highNum)+1,1):
+    #if (math.remainder(r,int(mult))) == 0:
+        #print (r)
